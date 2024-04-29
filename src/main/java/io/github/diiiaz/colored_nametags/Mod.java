894a6a9f1b069bfcfb50d5315ef1ad097a8e7649
diiiaz <cantonival@gmail.com>
2024-04-29 12:52:53 +0200

    Name Tags can now be cleaned via right-clicking water cauldron, up version to 1.0.1

--- src/main/java/io/github/diiiaz/colored_nametags/Mod.java
+++ src/main/java/io/github/diiiaz/colored_nametags/Mod.java
@@ -1,6 +1,8 @@
 package io.github.diiiaz.colored_nametags;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.cauldron.CauldronBehavior;
+import net.minecraft.item.Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +12,7 @@ public class Mod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		CauldronBehavior.WATER_CAULDRON_BEHAVIOR.put(Items.NAME_TAG, CauldronBehavior.CLEAN_DYEABLE_ITEM);
 //		ModRecipeSerializer.registerRecipes();
 	}
 }
